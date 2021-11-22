d78652809f02bf7583cacc92310c6de3bc8f3237
Berkan Büyük <berkanbyk55@gmail.com>
2021-11-22 19:51:12 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/aktuel_brosurler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aktuel_brosurler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AnaSayfa(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class AnaSayfa extends StatefulWidget {
+  const AnaSayfa({Key? key, required this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  State<AnaSayfa> createState() => _AnaSayfaState();
+}
+
+class _AnaSayfaState extends State<AnaSayfa> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          ],
+        ),
+      ),
+    );
+  }
+}
